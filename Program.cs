abab998a4065099063638ecef110703f6b9fba31
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-24 23:12:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace project04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true)
+            {
+                Console.Write("Enter first number=> "); int.TryParse(Console.ReadLine(), out int firstNum);
+                Console.Write("Enter second number=> "); int.TryParse(Console.ReadLine(), out int secondNum);
+
+                if (firstNum != secondNum) Console.WriteLine(firstNum + secondNum);
+                else Console.WriteLine((firstNum + secondNum) * 3);
+                Console.WriteLine();
+            }
+        }
+    }
+}
